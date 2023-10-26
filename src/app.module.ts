cf8e22448f8aa204b79af46950ae48fb7683bbec
franky-lim24 <franky.2021@scis.smu.edu.sg>
2023-10-26 22:16:24 +0800

    connect to elasticsearch

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogModule } from './log/log.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [LogModule],
+  imports: [LogModule, SearchModule],
   controllers: [AppController],
   providers: [AppService],
 })
