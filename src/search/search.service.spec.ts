51021ba06236d52dcfcb6142d4118afc2ad3fb40
franky-lim24 <franky.2021@scis.smu.edu.sg>
2023-11-06 01:37:21 +0800

    mock elasticsearch

--- src/search/search.service.spec.ts
+++ src/search/search.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SearchService } from './search.service';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 describe('SearchService', () => {
   let service: SearchService;
+  const mockElasticsearchService = {
+    search: jest.fn(),
+    index: jest.fn(),
+    delete: jest.fn(),
+    update: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SearchService],
+      providers: [
+        SearchService,
+        // Provide the mock implementation
+        { provide: ElasticsearchService, useValue: mockElasticsearchService },
+      ],
     }).compile();
 
     service = module.get<SearchService>(SearchService);
